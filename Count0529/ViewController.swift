ff91522112522736a1fe0351e169d16f651121ae
Rinazr <rinazr@rinazr-no-macbook-pro.local>
2016-05-29 11:38:06 +0900

    Initial Commit

---
+++ Count0529/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count0529
+//
+//  Created by Rinazr on 2016/05/29.
+//  Copyright © 2016年 Rinazr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
